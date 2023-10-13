efb7a3f22a3ed8a76ae839125f0279515031d4af
RaW <akram.iiuc.ctg@gmail.com>
2023-10-13 16:39:03 +0600

    Test

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#592abf",
+
+          secondary: "#19a085",
+
+          accent: "#ffea68",
+
+          neutral: "#302631",
+
+          "base-100": "#49484b",
+
+          info: "#6ba4eb",
+
+          success: "#5be197",
+
+          warning: "#e7b223",
+
+          error: "#df2a45",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
